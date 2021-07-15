38b7e3e6d5c8ffd758b9a96a86d5f4cbbd302a92
Snow-0 <69723902+Snow-0@users.noreply.github.com>
2021-07-15 09:33:22 -0400

    Add files via upload

--- dwmblocks/src/volume.sh
+++ dwmblocks/src/volume.sh
@@ -7,11 +7,11 @@ case $BLOCK_BUTTON in
 esac
 
 
-vol="$(pactl list sinks | grep '^[[:space:]]Volume:' |     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
+vol="$(pactl list sinks | grep '^[[:space:]]Volume:' |     head -n $(( $SINK + 2 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
 
-if [ "$vol" -gt "60" ]; then
+if [[ "$vol" -gt "60" ]]; then
 	icon="  "
-elif [ "$vol" -gt "20" ]; then
+elif [[ "$vol" -gt "20" ]]; then
 	icon="  "
 else
 	icon="  "
